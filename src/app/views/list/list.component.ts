60ef89f6e14cb9d64e825687963722ba90abb357
estelamaga <estelinha.mag@hotmail.com>
2019-07-27 14:50:59 -0300

    projeto com conceitos introdutórios do angular

---
+++ src/app/views/list/list.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { PokeapiService } from 'src/app/service/pokeapi.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+
+  nameFilter = "";
+
+  // Armazenar o pokemon
+  selectedPkm = null;
+ 
+  get pokemonList() {
+    return this.pokeapi.pokeList.filter(pokemon => {
+      return pokemon.name.toLowerCase().indexOf(this.nameFilter.toLowerCase()) !== -1;
+    })
+  }
+
+  get pkmSprite(){
+    const number = ('000' + this.selectedPkm.number).slice(-3);
+    return `//serebii.net/sunmoon/pokemon/${number}.png`;
+  }
+
+
+  constructor(
+    private pokeapi: PokeapiService
+  ) { }
+
+  ngOnInit() {
+    this.pokeapi.listAll();
+  }
+
+  selectPokemon(pkm){
+    this.selectedPkm = pkm;
+  }
+
+}
